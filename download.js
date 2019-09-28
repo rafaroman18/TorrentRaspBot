4ea60cb434dd590428bb47712e25afacb1f3ca44
rafaroman18 <rafaroman182@gmail.com>
2019-09-28 16:55:19 +0200

    Testing HTTPS 😱

--- download.js
+++ download.js
@@ -14,9 +14,7 @@ const download = (ctx) => {
         
         var download = function(uri, filename, callback){
             request.head(uri, function(err, res, body){
-                console.log('content-type:', res.headers['content-type']);
-                console.log('content-length:', res.headers['content-length']);
-
+                
                 request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
             });
         };
