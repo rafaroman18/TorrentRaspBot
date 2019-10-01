0d5efc796eac4ee5e3dd4073ddd603097000e19b
rafaroman18 <rafaroman182@gmail.com>
2019-10-01 17:48:03 +0200

    ALMOST THERE 🤗

--- download.js
+++ download.js
@@ -36,7 +36,7 @@ const download = (ctx) => {
         ctx.reply('Downloading...')
         DWNLD(ctx.command.args[0]).then(() => { //We call the function
             ctx.reply('Downloaded!').then(() => { //If it is successful, reply 'Downloaded!'
-            var stdout = 'NO'
+            
                 const { stdout, stderr, code } = shell.exec('file -b /home/pi/TRB/tempDownload/file', { silent: true }, { async: true })
                     stdout.replace(/\n/g, '')
                     stdout.replace(/\t/g, '')
