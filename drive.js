79e0979d1b974963e5e6034210d9e51718fa1ff7
rafaroman18 <rafaroman182@gmail.com>
2019-10-28 23:37:12 +0100

    Trying again to finish this project

--- drive.js
+++ drive.js
@@ -3,7 +3,7 @@ const shell = require('shelljs')
 async function drive(ctx,name){
 
     await ctx.reply('Uploading the file to Google Drive!')
-    //const { stdout, stderr, code } = await shell.exec('file -b /home/pi/TRB/tempDownload/'+name, { silent: true }, { async: true })
+    const { stdout, stderr, code } = await shell.exec('file -b /home/pi/TRB/tempDownload/'+name, { silent: true }, { async: true })
     return new Promise((resolve,reject)=>{
         if(stderr){
             ctx.reply('An error has ocurred uploading the file to Google Drive.')
