a914165f577dde10e511e57cbe5def6832410a4a
rafaroman18 <rafaroman182@gmail.com>
2019-10-29 23:13:18 +0100

    Introducing Transmission Commands��

--- torrent.js
+++ torrent.js
@@ -3,8 +3,11 @@ var shell = require('shelljs')
 async function torrent(ctx,name) {
     await ctx.reply("Torrent File detected. Starting Transmission")
     try{
-        await ctx.reply('You have reached torrent function. Congrats!')
-        //const { stdout, stderr, code } = await shell.exec('transmission-remote -a ', { silent: true }, { async: true })
+        const { stdout, stderr, code } = await shell.exec('transmission-remote -n \'transmission:transmission\' -a /tempDownload/'+name, { silent: true }, { async: true })
+        await ctx.reply('Torrent file added.')
+
+        await ctx.reply('Starting Torrent.')
+        const { stdout, stderr, code } = await shell.exec('transmission-remote -n \'transmission:transmission\' -t 1 -s', { silent: true }, { async: true })
 
     }catch(error){
         console.log(error)
