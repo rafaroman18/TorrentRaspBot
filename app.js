22df4d86544f2fa13040317ade851e3a9e40da7a
rafaroman18 <rafaroman182@gmail.com>
2019-10-30 12:57:08 +0100

    Introducing Transmission Commands ��

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ bot.use(commandArgs) //Divide the message chat into raw/command/args
 bot.start((ctx) => ctx.reply('Welcome to TorrentRaspBot! \nWrite /help to see the available commands!'))
 bot.help((ctx) => ctx.reply('These are the available commands:\n/download "url file" "name file" -> Downloads the file and automatically uploads it to Google Drive\n/author -> Shows the author and some info of him'))
 bot.command('author', (ctx) => ctx.reply('Author: Rafael Roman \nGithub: github.com/rafaroman18 \n2019'))
-bot.command('download', download)
+bot.command('download')//, download)
 bot.command('update', update)
 
 bot.launch()
